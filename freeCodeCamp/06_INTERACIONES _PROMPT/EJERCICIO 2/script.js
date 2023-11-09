081158d93e129427dfc5aff2aa54d4489b110322
karo123vargas <ka.vargaspavez@gmail.com>
2023-11-09 15:58:13 -0300

    ok

--- freeCodeCamp/06_INTERACIONES _PROMPT/EJERCICIO 2/script.js
+++ freeCodeCamp/06_INTERACIONES _PROMPT/EJERCICIO 2/script.js
@@ -1,13 +1,12 @@
 //vereficar la temperatura 
-function temperatura(){
+function temp(){
     let temp =prompt("por favor muestrame la temperatura : ")
 if( temp < 0){
     alert("Hace mucho frio : " + temp + "°C");
 }else if (temp > 0 && temp < 25){
     alert("La temperatura es agradable :"  + temp + "°C");
 } 
-    else{
+    else {
         alert("Hace calor: " + temp + "°C");
     }
 }
-
