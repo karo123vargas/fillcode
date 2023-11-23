71c332d9af33c736f232606efb6578ba86b45e80
karo123vargas <ka.vargaspavez@gmail.com>
2023-11-23 12:52:15 -0300

    ok

---
+++ freeCodeCamp/07_bucles_while_for/01_Ejercicio_while/Ejercicio_1/script.js
@@ -0,0 +1,10 @@
+// Desarrollo Ejercicio
+
+function contar(){let contador = 20;
+    let array = [];
+    while(contador >= 10){
+        array.push(contador);
+        contador = --;
+    }
+    alert(`Lista: ${array}`);
+}
